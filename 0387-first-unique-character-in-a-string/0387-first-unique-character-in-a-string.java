5e129b87364d9685caa0190d8bba6192c78762f0
Anushka Lakhera <160614406+Anushkalakhera@users.noreply.github.com>
2026-04-01 16:34:44 +0530

    Time: 32 ms (30.18%), Space: 46.7 MB (94.71%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -7,11 +7,9 @@ class Solution {
         }
 
         for(int i=0;i<s.length();i++){
-            if(map.containsKey(s.charAt(i))){
                 if(map.get(s.charAt(i))==1){
                     return i;
                 }
-            }
         }
         return -1;
     }
