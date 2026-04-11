1b91f02bad23b7957b56f269fa6466450505af3d
Anushka Lakhera <160614406+Anushkalakhera@users.noreply.github.com>
2026-04-11 10:21:55 +0530

    Time: 0 ms (100%), Space: 43.1 MB (88.49%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
@@ -1,6 +1,5 @@
 class Solution {
     public int dominantIndex(int[] nums) {
-        int index=-1;
         for(int i=0;i<nums.length;i++){
             boolean check=true;
             for(int j=0;j<nums.length;j++){
@@ -10,9 +9,9 @@ class Solution {
                 }
             }
             if(check==true){
-                index=i;
+                return i;
             }
         }  
-        return index;      
+        return -1;      
     }
 }
