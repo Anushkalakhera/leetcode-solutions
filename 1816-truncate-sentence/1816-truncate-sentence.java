463c0a7a444ab9bec3d12aa4ff07e520721989ea
Anushka Lakhera <160614406+Anushkalakhera@users.noreply.github.com>
2026-04-15 13:07:48 +0530

    Time: 4 ms (15.91%), Space: 44.4 MB (6.21%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int count=0;
+        String ans="";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                count++;
+            }
+            ans+=s.charAt(i);
+            if(count==k) break;
+        }
+        return ans.trim();
+    }
+}
