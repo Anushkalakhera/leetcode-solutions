44373c18746986659b28ec7e251588d9ee863ca3
Anushka Lakhera <160614406+Anushkalakhera@users.noreply.github.com>
2026-04-17 17:51:10 +0530

    Time: 1 ms (100%), Space: 46.5 MB (32.74%) - LeetHub

---
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int vowelStrings(String[] words, int left, int right) {
+        int count=0;
+        for(int i=left;i<=right;i++){
+            String s=words[i];
+            int n=s.length();
+            if(isVowel(s.charAt(0)) && isVowel(s.charAt(n-1))){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean isVowel(char s){
+        char[] vowels={'a','e','i','o','u'};
+        for(int i=0;i<vowels.length;i++){
+            if(s==vowels[i]) return true;
+        }
+
+        return false;
+    }
+}
