60896c1b5a546a14ec63fd5f60f39c298856ecf7
Anushka Lakhera <160614406+Anushkalakhera@users.noreply.github.com>
2026-06-10 14:01:17 +0530

    Time: 0 ms (100%), Space: 47.9 MB (63.79%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,12 @@
 class Solution {
     public double myPow(double x, int n) {
-       return Math.pow(x,n);
+        return helper(x,(long)n);
+    }
+
+    public double helper(double x,long n){
+        if(n==0) return 1;
+        if(n<0) return helper(1/x,-n);
+        if(n%2==0) return helper(x*x,n/2);
+        return x*helper(x*x,(n-1)/2);
     }
 }
