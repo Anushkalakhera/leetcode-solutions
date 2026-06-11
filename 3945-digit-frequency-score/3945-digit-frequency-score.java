765475602c59bd2397f45086ea73bb251330dcf5
Anushka Lakhera <160614406+Anushkalakhera@users.noreply.github.com>
2026-06-11 10:43:09 +0530

    Time: 2 ms (32.04%), Space: 43.2 MB (7.19%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        while(n!=0){
+            int rem=n%10;
+            map.put(rem,map.getOrDefault(rem,0)+1);
+            n=n/10;
+        }
+        int ans=0;
+        for(int key : map.keySet()){
+            ans+=key*map.get(key);
+        }
+        return ans;
+    }
+}
