cd6f4fd1107d89f4af53b14e883896e48dc2779a
Anushka Lakhera <anushkalakera540@gmail.com>
2026-06-20 22:25:29 +0530

    Time: 10 ms (34.42%), Space: 42.3 MB (59.13%) - LeetHub

---
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+       Set<Integer> primes = Set.of(2, 3, 5, 7, 11, 13, 17, 19);
+
+        int count = 0;
+
+        for (int num = left; num <= right; num++) {
+            int setBits = Integer.bitCount(num);
+
+            if (primes.contains(setBits)) {
+                count++;
+            }
+        }
+
+        return count; 
+    }
+}
