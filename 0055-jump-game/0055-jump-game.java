8f66e8907f9ac62b3643b4b55b60ba0128ce4a66
Anushka Lakhera <anushkalakera540@gmail.com>
2026-07-02 14:50:24 +0530

    Time: 2 ms (88.73%), Space: 47.9 MB (62.41%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,10 @@
 class Solution {
+    
     public boolean canJump(int[] nums) {
-        int maxJump=0;
+        int max=0;
         for(int i=0;i<nums.length;i++){
-            if(i>maxJump) return false;
-            maxJump=Math.max(maxJump,i+nums[i]);
+            if(i>max) return false;
+            max=Math.max(max,i+nums[i]);
         }
         return true;
     }
