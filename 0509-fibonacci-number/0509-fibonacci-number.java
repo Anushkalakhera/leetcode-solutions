f52e0e470aef73dd340c81855bcc3a6b8078ab38
Anushka Lakhera <anushkalakera540@gmail.com>
2026-07-02 14:05:24 +0530

    Time: 0 ms (100%), Space: 42.4 MB (15.76%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,6 +2,7 @@ class Solution {
     int[] dp;
     public int calc(int n){
         if(n==0 || n==1) return n;
+        if(dp[n]!=-1) return dp[n];
         int left=calc(n-1);
         int right=calc(n-2);
         return dp[n]=left+right;
