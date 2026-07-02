35efb6d5124c4b18f9bd01d4c3498928ceff64ba
Anushka Lakhera <anushkalakera540@gmail.com>
2026-07-02 14:03:38 +0530

    Time: 17 ms (8.73%), Space: 42.2 MB (33.25%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    int[] dp;
+    public int calc(int n){
+        if(n==0 || n==1) return n;
+        int left=calc(n-1);
+        int right=calc(n-2);
+        return dp[n]=left+right;
+    }
+    public int fib(int n) {
+        dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return calc(n);
+    }
+}
