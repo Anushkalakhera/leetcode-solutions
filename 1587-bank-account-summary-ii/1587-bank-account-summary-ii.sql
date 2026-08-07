c3246bb08912abc8616f26bf94e876b3e9b1c2a9
Anushka Lakhera <anushkalakera540@gmail.com>
2026-08-07 19:34:33 +0530

    Time: 821 ms (72.9%), Space: 0B (100%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -3,5 +3,5 @@ select name,sum(amount) as balance
 from Users
 join Transactions
 on Users.account=Transactions.account
-group by Transactions.account
+group by users.account,users.name
 having sum(amount)>10000;
