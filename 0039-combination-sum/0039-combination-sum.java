7d15517811a93f3815e49a60bc289dad0f65af45
Anushka Lakhera <anushkalakera540@gmail.com>
2026-08-27 21:32:07 +0530

    Time: 2 ms (95.72%), Space: 45.9 MB (33.53%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,24 @@
 class Solution {
-    public void backTrack(List<List<Integer>> finalList,List<Integer> currList,int[] nums,int start,int target){
-        if(target<0) return;
-        if(target==0){
+    
+    public void backTrack(List<List<Integer>> finalList,List<Integer> currList,int[] nums,int i,int ans,int target) {
+        if (ans == target) {
             finalList.add(new ArrayList<>(currList));
             return;
-        }else{
-            for(int i=start;i<nums.length;i++){
-                currList.add(nums[i]);
-                backTrack(finalList,currList,nums,i,target-nums[i]);
-                currList.remove(currList.size()-1);
-           }
         }
-        
+
+        if (i >= nums.length || ans > target) {
+            return;
+        }
+
+        currList.add(nums[i]);
+        backTrack(finalList,currList,nums,i,ans+nums[i],target);
+        currList.remove(currList.size()-1);
+        backTrack(finalList,currList,nums,i+1,ans,target);
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> finalList=new ArrayList<>();
-        Arrays.sort(candidates);
-        backTrack(finalList,new ArrayList<>(),candidates,0,target);
-        return finalList;      
+        List<List<Integer>> finalList = new ArrayList<>();
+        backTrack(finalList,new ArrayList<>(),candidates,0,0,target);
+        return finalList;
     }
 }
