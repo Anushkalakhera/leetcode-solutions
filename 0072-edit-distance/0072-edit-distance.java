c09cb7ead3196280bc1b4df35f58854c2d261c39
Anushka Lakhera <anushkalakera540@gmail.com>
2026-09-01 15:20:57 +0530

    Time: 7 ms (3.04%), Space: 46.6 MB (91.57%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -24,6 +24,38 @@ class Solution {
 
         return dp[i][j];
     }
+
+
+
+    // by tabulation 
+    public int solveTab(String a,String b){
+        int[][] dp=new int[a.length()+1][b.length()+1];
+
+        for(int j=0;j<=b.length();j++){
+            dp[a.length()][j]=b.length()-j;
+        }
+        for(int i=0;i<=a.length();i++){
+            dp[i][b.length()]=a.length()-i;
+        }
+
+        for(int i=a.length()-1;i>=0;i--){
+            for(int j=b.length()-1;j>=0;j--){
+                int ans=0;
+                if(a.charAt(i)==b.charAt(j)){
+                    ans=dp[i+1][j+1];
+                }else{
+                    int insertAns=1+dp[i][j+1];
+                    int delAns=1+dp[i+1][j];
+                    int replaceAns=1+dp[i+1][j+1];
+                    
+                    ans=Math.min(insertAns,Math.min(delAns,replaceAns));
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
+    }
+
     public int minDistance(String word1, String word2) {
         int[][] dp=new int[word1.length()][word2.length()];
         for(int i=0;i<word1.length();i++){
@@ -31,7 +63,8 @@ class Solution {
                 dp[i][j]=-1;
             }
         }
-        int ans=solve(word1,word2,0,0,dp);
+        // int ans=solve(word1,word2,0,0,dp);
+        int ans=solveTab(word1,word2);
         return ans;
     }
 }
